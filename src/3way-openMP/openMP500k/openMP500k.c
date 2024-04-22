283018e79643237a5473e7d701f18920fe9bf632
Alex8974 <ajbingh2@ksu.edu>
2024-04-21 21:13:29 -0500

    Better openMP

--- src/3way-openMP/openMP500k/openMP500k.c
+++ src/3way-openMP/openMP500k/openMP500k.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <sys/time.h>
+//#include <sys/resource.h>
 
-#define NUMLINES 500000
+#define NUMLINES 1000000
 
-int line_array[NUMLINES]; // holds ASCII values found
+int line_array[NUMLINES]; // holds the ASCII values found
 
 int find_largest_ascii(char *line, int length) {
     int max_val = 0;
@@ -29,7 +31,7 @@ void get_largest_ascii(int start, int end) {
     fseek(fd, start, SEEK_SET); // moves to start position
 
     for (int i = start; i < end; i++) {
-        getline(&line, &len, fd);
+        fgets(line, len, fd);
         line_array[i] = find_largest_ascii(line, len);
     }
 
@@ -38,7 +40,7 @@ void get_largest_ascii(int start, int end) {
 
 void print_results() {
     for (int i = 0; i < NUMLINES; i++) {
-        printf("%d: %d\n", i, line_array[i]); // prints each line
+        printf("%d: %d\n", i, line_array[i]); // prints each line number and its largest ASCII value
     }
 }
 
@@ -47,6 +49,11 @@ int main() {
     int chunk_size = NUMLINES / num_threads;
 
     printf("Starting on %s\n", getenv("HOSTNAME"));
+
+    struct timeval t1, t2;
+    double elapsedTime;
+
+    gettimeofday(&t1, NULL);
 
     #pragma omp parallel num_threads(num_threads)
     {
@@ -57,7 +64,13 @@ int main() {
         get_largest_ascii(start, end);
     }
 
+    gettimeofday(&t2, NULL);
+    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; //Converted to milliseconds
+    elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
+
     print_results();
+    printf("\nNumber of Threads: %d\n", num_threads);
+    printf("Total Execution Time: %fms\n", elapsedTime);
     printf("Main: program completed. Exiting.\n");
 
     return 0;
